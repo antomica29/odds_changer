be72eb6fec31c3dcb0f039ca975bc91c846d9c82
antomica29 <45823586+antomica29@users.noreply.github.com>
2022-02-08 02:13:13 +0100

    inserting html to front end

--- wp-content/plugins/odds_changer/index.php
+++ wp-content/plugins/odds_changer/index.php
@@ -5,42 +5,68 @@
  * Author: anton
  **/
 
-//adding page to dashboard menu
-function addPluginAdminMenu() {
-    add_menu_page(  "Odds Changer", 'Odds Changer', 'administrator', "Odds Changer", 'displaySettingsPage', 'dashicons-sos', 26 );
+function odds_changer_initPlugin()
+{
+    //adding page to dashboard menu
+    add_menu_page("Odds Changer", 'Odds Changer', 'administrator', "Odds Changer", 'displaySettingsPage', 'dashicons-sos', 26);
+
+    //showing front end component if plugin active
+    if (function_exists('displaySettingsPage')) {
+        odds_changer_inject();
+    }
 }
-add_action('admin_menu', 'addPluginAdminMenu', 9);
+add_action('admin_menu', 'odds_changer_initPlugin', 9);
+
+function odds_changer_inject()
+{
+    if(!is_admin()) {
+        require_once 'partials/odds_change_component.php';
+    }
+}
+add_action('get_footer', 'odds_changer_inject');
+
+function odds_changer_enqueue_script() {
+    wp_enqueue_style( 'odds_changer_style', get_stylesheet_uri() );
+    wp_enqueue_script( 'odds_changer_script', plugin_dir_url( __FILE__ ) . 'src/script.js' );
+}
+add_action('wp_enqueue_scripts', 'odds_changer_enqueue_script');
 
 //displaying form
-function displaySettingsPage() {
+function displaySettingsPage()
+{
     require_once 'partials/settings.php';
 }
 
 //registering sections and text fields
-function register_settings() {
-    register_setting( 'odds_changer_options', 'odds_changer_setting_odds_class');
-    add_settings_section( 'odds_changer_settings', 'Odds Changer Settings', 'odds_changer_section_text_callback', 'odds_changer' );
+function register_settings()
+{
+    register_setting('odds_changer_options', 'odds_changer_setting_odds_class');
+    add_settings_section('odds_changer_settings', 'Odds Changer Settings', 'odds_changer_section_text_callback', 'odds_changer');
 
-    add_settings_field( 'odds_changer_setting_odds_class', 'Odds Class', 'odds_changer_setting_odds_class', 'odds_changer', 'odds_changer_settings' );
+    add_settings_field('odds_changer_setting_odds_class', 'Odds Class', 'odds_changer_setting_odds_class', 'odds_changer', 'odds_changer_settings');
 }
-add_action( 'admin_init', 'register_settings' );
 
-function odds_changer_section_text_callback() {
+add_action('admin_init', 'register_settings');
+
+function odds_changer_section_text_callback()
+{
     echo 'Put the class name of the odds to change.';
 }
 
 //text field settings for class name
-function odds_changer_setting_odds_class() {
-    $settings = (array) get_option( 'odds_changer_setting_odds_class' );
+function odds_changer_setting_odds_class()
+{
+    $settings = (array)get_option('odds_changer_setting_odds_class');
     $field = "odds_class";
-    $value = esc_attr( $settings[$field] );
+    $value = esc_attr($settings[$field]);
 
     echo "<input type='text' name='odds_changer_setting_odds_class[$field]' value='$value' />";
 }
 
 //input for field in wp_options
-function odds_changer_options_submit($input ){
-    $output['odds_class'] = sanitize_text_field( $input ['odds_class'] );
+function odds_changer_options_submit($input)
+{
+    $output['odds_class'] = sanitize_text_field($input ['odds_class']);
 
     return $output;
 }
---
+++ wp-content/plugins/odds_changer/partials/odds_change_component.php
@@ -0,0 +1,6 @@
+<div class="odds_checker_component">
+    <span>choose odds format</span>
+    <div class="odds_format_decimal">decimal</div>
+    <div class="odds_format_fractal">fractal</div>
+    <div class="odds_format_american">american</div>
+</div>
