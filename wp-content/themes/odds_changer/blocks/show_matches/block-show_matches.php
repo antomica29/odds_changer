9710a9cc0b9ee769b1b00fb162f8cc4229f4adc9
antomica29 <45823586+antomica29@users.noreply.github.com>
2022-02-11 02:21:30 +0100

    sorting function ready

--- wp-content/themes/odds_changer/blocks/show_matches/block-show_matches.php
+++ wp-content/themes/odds_changer/blocks/show_matches/block-show_matches.php
@@ -25,42 +25,81 @@ for ($i = 0; $i < count($posts); $i++) {
     $title = get_field('home_team', $posts[$i]->ID) . " VS " . get_field('away_team', $posts[$i]->ID);
     $sites = get_field('sites', $posts[$i]->ID);
 
-    $partner_nice = $sites[0]['site_nice'];
+    $best_home_name = "";
+    $best_home = 0;
+    $best_draw_name = "";
+    $best_draw = 0;
+    $best_away_name = "";
+    $best_away = 0;
+
+    for ($j = 0; $j < count($sites); $j++) {
+
+        //iterate also odds_type, but gonna hard code it
+        if($sites[$j]['odds'][0]['home_odds'] > $best_home){
+            $best_home_name = $sites[$j]['site_nice'];
+            $best_home = $sites[$j]['odds'][0]['home_odds'];
+        }
+
+        if($sites[$j]['odds'][0]['draw_odds'] > $best_draw){
+            $best_draw_name = $sites[$j]['site_nice'];
+            $best_draw = $sites[$j]['odds'][0]['draw_odds'];
+        }
+
+        if($sites[$j]['odds'][0]['away_odds'] > $best_away){
+            $best_away_name = $sites[$j]['site_nice'];
+            $best_away = $sites[$j]['odds'][0]['away_odds'];
+        }
+    }
+
+    $home_partner_nice = $best_home_name;
+    $draw_partner_nice = $best_draw_name;
+    $away_partner_nice = $best_away_name;
     $odds_type = $sites[0]['odds'][0]['odds_type'];
-    $odds_home = $sites[0]['odds'][0]['home_odds'];
-    $odds_draw = $sites[0]['odds'][0]['draw_odds'];
-    $odds_away = $sites[0]['odds'][0]['away_odds'];
+    $odds_home = $best_home;
+    $odds_draw = $best_draw;
+    $odds_away = $best_away;
 
     ?>
 
     <div class="match_row">
         <span class="match_row__odds_type"><?= $odds_type ?></span>
-        <div class="match_row__title">
-            <?= $title ?>
-        </div>
+        <a href="<?= get_permalink($posts[$i]->ID) ?>">
+            <div class="match_row__title">
+                <?= $title ?>
+            </div>
+        </a>
         <div class="match_row__odds_row">
             <div class="match_row__odds_row_container">
                 <div class="match_row__odds_row_partner">
-                    <?= $partner_nice ?>
+                    <?= $home_partner_nice ?>
                 </div>
                 <div class="match_row__odds_row_odds odds">
                     <?= $odds_home ?>
                 </div>
+                <div class="match_row__odds_row_team">
+                    home
+                </div>
             </div>
             <div class="match_row__odds_row_container">
                 <div class="match_row__odds_row_partner">
-                    <?= $partner_nice ?>
+                    <?= $draw_partner_nice ?>
                 </div>
                 <div class="match_row__odds_row_odds odds">
                     <?= $odds_draw ?>
                 </div>
+                <div class="match_row__odds_row_team">
+                    draw
+                </div>
             </div>
             <div class="match_row__odds_row_container">
                 <div class="match_row__odds_row_partner">
-                    <?= $partner_nice ?>
+                    <?= $away_partner_nice ?>
                 </div>
                 <div class="match_row__odds_row_odds odds">
                     <?= $odds_away ?>
+                </div>
+                <div class="match_row__odds_row_team">
+                    away
                 </div>
             </div>
         </div>
